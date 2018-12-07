252e5803eb02da5f93c81c501045a61edd74eb6b
Christophe <cavo789@yahoo.com>
2018-12-07 17:53:38 +0100

    Animated gif

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
-/**
+/*
  * AUTHOR : AVONTURE Christophe
  *
  * Written date : 3 october 2018
@@ -24,291 +24,292 @@ define('REPO', 'https://github.com/cavo789/marknotes_csv2md');
 
 class CSVTable
 {
-	public function __construct(
-		string $csv,
-		string $delim = ',',
-		string $enclosure = '"',
-		string $table_separator = '|',
-		bool $transpose = false
-	) {
-		$this->csv = $csv;
-		$this->delim = $delim;
-		$this->enclosure = $enclosure;
-		$this->table_separator = $table_separator;
+    public function __construct(
+        string $csv,
+        string $delim = ',',
+        string $enclosure = '"',
+        string $table_separator = '|',
+        bool $transpose = false
+    ) {
+        $this->csv             = $csv;
+        $this->delim           = $delim;
+        $this->enclosure       = $enclosure;
+        $this->table_separator = $table_separator;
 
-		// Fill the rows with Markdown output
-		$this->header = ''; 	// Table header
-		$this->rows = ''; 	// Table rows
+        // Fill the rows with Markdown output
+        $this->header = ''; 	// Table header
+        $this->rows   = ''; 	// Table rows
 
-		$this->CSVtoTable($transpose);
-	}
+        $this->CSVtoTable($transpose);
+    }
 
-	/**
-	 * Transpose a two-dimensional array
-	 *
-	 * ### Example
-	 *
-	 * We've an array by user and, for each user, we have a question and
-	 * the answer.
-	 *
-	 * $in = [
-	 * 	'User1' => [
-	 * 		'Question1' => 'Answer User1 - Q1',
-	 * 		'Question2' => 'Answer User1 - Q2',
-	 * 		'Question3' => 'Answer User1 - Q3'
-	 * 	],
-	 * 	'User2' => [
-	 * 		'Question1' => 'Answer User2 - Q1',
-	 * 		'Question2' => 'Answer User2 - Q2',
-	 * 		'Question3' => 'Answer User2 - Q3'
-	 * 	],
-	 * 	'User3' => [
-	 * 		'Question1' => 'Answer User3 - Q1',
-	 * 		'Question2' => 'Answer User3 - Q2',
-	 * 		'Question3' => 'Answer User3 - Q3'
-	 * 	]
-	 * ];
-	 *
-	 * We can transpose the array to have first the question then
-	 * the answer given to that question by each user.
-	 *
-	 * So User->Question->Answer should become Question->User->Answer
-	 *
-	 * $out = Transpose($in);
-	 *
-	 * This will give:
-	 *
-	 * $out = [
-	 *		'Question1' => [
-	 *			'User1' => 'Answer User1 - Q1',
-	 *			'User2' => 'Answer User2 - Q1',
-	 *			'User3' => 'Answer User3 - Q1'
-	 *		],
-	 *		'Question2' => [
-	 *			'User1' => 'Answer User1 - Q2',
-	 *			'User2' => 'Answer User2 - Q2',
-	 *			'User3' => 'Answer User3 - Q2'
-	 *		],
-	 *		'Question3' => [
-	 *			'User1' => 'Answer User1 - Q3',
-	 *			'User2' => 'Answer User2 - Q3',
-	 *			'User3' => 'Answer User3 - Q3'
-	 *		]
-	 *	]
-	 *
-	 *
-	 * @link https://stackoverflow.com/questions/797251/transposing-multidimensional-arrays-in-php/797268#797268
-	 *
-	 * @param  array $arr
-	 * @return array
-	 */
-	private function transpose(array $arr) : array
-	{
-		$out = [];
-		foreach ($arr as $key => $subarr) {
-			foreach ($subarr as $subkey => $subvalue) {
-				$out[$subkey][$key] = $subvalue;
-			}
-		}
+    /**
+     * Convert the CSV into a PHP array.
+     *
+     * @param string $csv Convert a string into a PHP array
+     *
+     * @return array
+     */
+    public function toArray(string $csv): array
+    {
+        $parsed = str_getcsv($csv, "\n"); // Parse the rows
+        $output = [];
+        foreach ($parsed as &$row) {
+            $row = str_getcsv($row, $this->delim, $this->enclosure); // Parse the items in rows
+            array_push($output, $row);
+        }
 
-		return $out;
-	}
+        return $output;
+    }
 
-	/**
-	 * Convert a string into an array
-	 *
-	 * @param  boolean $transpose True if the orientation of the table
-	 *                            should be changed. Works ONLY when the table
-	 *                            is composed of two rows.
-	 * @return void
-	 */
-	private function CSVtoTable(bool $transpose = false)
-	{
-		$parsed_array = $this->toArray($this->csv);
+    public function getMarkup(): string
+    {
+        return $this->header . $this->rows;
+    }
 
-		// Transpose only when the array has exactly two rows
-		if ((count($parsed_array) == 2) && ($transpose)) {
-			$parsed_array = self::transpose($parsed_array);
-			// Add, as the first row, a heading row
-			$arrHeader = ['code', 'value'];
-			array_unshift($parsed_array, $arrHeader);
-		}
+    protected function createRows(array $rows): string
+    {
+        $output = '';
+        foreach ($rows as $row) {
+            $output .= $this->createRow($row);
+        }
 
-		$this->length = $this->minRowLength($parsed_array);
-		$this->col_widths = $this->maxColumnWidths($parsed_array);
-		$header_array = array_shift($parsed_array);
-		$this->header = $this->createHeader($header_array);
-		$this->rows = $this->createRows($parsed_array);
-	}
+        return $output;
+    }
 
-	/**
-	 * Convert the CSV into a PHP array
-	 *
-	 * @param  string $csv Convert a string into a PHP array
-	 * @return array
-	 */
-	public function toArray(string $csv) : array
-	{
-		$parsed = str_getcsv($csv, "\n"); // Parse the rows
-		$output = [];
-		foreach ($parsed as &$row) {
-			$row = str_getcsv($row, $this->delim, $this->enclosure); // Parse the items in rows
-			array_push($output, $row);
-		}
+    protected function createRow(array $row): string
+    {
+        $output = $this->table_separator . ' ';
+        // Only create as many columns as the minimal number of elements
+        // in all rows. Otherwise this would not be a valid Markdown table
+        for ($i = 0; $i < $this->length; ++$i) {
+            $element = $this->padded(trim($row[$i]), $this->col_widths[$i]);
+            $output .= $element;
+            $output .= ' ' . $this->table_separator . ' ';
+        }
 
-		return $output;
-	}
+        $output = trim($output) . "\n"; // row ends with a newline
+        return $output;
+    }
 
-	private function createHeader(array $header_array) : string
-	{
-		return $this->createRow($header_array) . $this->createSeparator();
-	}
+    /**
+     * Transpose a two-dimensional array.
+     *
+     * ### Example
+     *
+     * We've an array by user and, for each user, we have a question and
+     * the answer.
+     *
+     * $in = [
+     * 	'User1' => [
+     * 		'Question1' => 'Answer User1 - Q1',
+     * 		'Question2' => 'Answer User1 - Q2',
+     * 		'Question3' => 'Answer User1 - Q3'
+     * 	],
+     * 	'User2' => [
+     * 		'Question1' => 'Answer User2 - Q1',
+     * 		'Question2' => 'Answer User2 - Q2',
+     * 		'Question3' => 'Answer User2 - Q3'
+     * 	],
+     * 	'User3' => [
+     * 		'Question1' => 'Answer User3 - Q1',
+     * 		'Question2' => 'Answer User3 - Q2',
+     * 		'Question3' => 'Answer User3 - Q3'
+     * 	]
+     * ];
+     *
+     * We can transpose the array to have first the question then
+     * the answer given to that question by each user.
+     *
+     * So User->Question->Answer should become Question->User->Answer
+     *
+     * $out = Transpose($in);
+     *
+     * This will give:
+     *
+     * $out = [
+     *		'Question1' => [
+     *			'User1' => 'Answer User1 - Q1',
+     *			'User2' => 'Answer User2 - Q1',
+     *			'User3' => 'Answer User3 - Q1'
+     *		],
+     *		'Question2' => [
+     *			'User1' => 'Answer User1 - Q2',
+     *			'User2' => 'Answer User2 - Q2',
+     *			'User3' => 'Answer User3 - Q2'
+     *		],
+     *		'Question3' => [
+     *			'User1' => 'Answer User1 - Q3',
+     *			'User2' => 'Answer User2 - Q3',
+     *			'User3' => 'Answer User3 - Q3'
+     *		]
+     *	]
+     *
+     *
+     * @see https://stackoverflow.com/questions/797251/transposing-multidimensional-arrays-in-php/797268#797268
+     *
+     * @param array $arr
+     *
+     * @return array
+     */
+    private function transpose(array $arr): array
+    {
+        $out = [];
+        foreach ($arr as $key => $subarr) {
+            foreach ($subarr as $subkey => $subvalue) {
+                $out[$subkey][$key] = $subvalue;
+            }
+        }
 
-	private function createSeparator() : string
-	{
-		$output = $this->table_separator . ' ';
-		for ($i = 0; $i < $this->length; ++$i) {
-			$output .= str_repeat('-', $this->col_widths[$i]);
-			$output .= ' ' . $this->table_separator . ' ';
-		}
+        return $out;
+    }
 
-		return trim($output) . "\n";
-	}
+    /**
+     * Convert a string into an array.
+     *
+     * @param bool $transpose True if the orientation of the table
+     *                        should be changed. Works ONLY when the table
+     *                        is composed of two rows.
+     *
+     * @return void
+     */
+    private function CSVtoTable(bool $transpose = false)
+    {
+        $parsed_array = $this->toArray($this->csv);
 
-	protected function createRows(array $rows) : string
-	{
-		$output = '';
-		foreach ($rows as $row) {
-			$output .= $this->createRow($row);
-		}
+        // Transpose only when the array has exactly two rows
+        if ((2 == count($parsed_array)) && ($transpose)) {
+            $parsed_array = self::transpose($parsed_array);
+            // Add, as the first row, a heading row
+            $arrHeader = ['code', 'value'];
+            array_unshift($parsed_array, $arrHeader);
+        }
 
-		return $output;
-	}
+        $this->length     = $this->minRowLength($parsed_array);
+        $this->col_widths = $this->maxColumnWidths($parsed_array);
+        $header_array     = array_shift($parsed_array);
+        $this->header     = $this->createHeader($header_array);
+        $this->rows       = $this->createRows($parsed_array);
+    }
 
-	/**
-	 * Add padding to a string
-	 */
-	private function padded(string $str, int $width) : string
-	{
-		if ($width < strlen($str)) {
-			return $str;
-		}
-		$padding_length = $width - strlen($str);
-		$padding = str_repeat(' ', $padding_length);
+    private function createHeader(array $header_array): string
+    {
+        return $this->createRow($header_array) . $this->createSeparator();
+    }
 
-		return $str . $padding;
-	}
+    private function createSeparator(): string
+    {
+        $output = $this->table_separator . ' ';
+        for ($i = 0; $i < $this->length; ++$i) {
+            $output .= str_repeat('-', $this->col_widths[$i]);
+            $output .= ' ' . $this->table_separator . ' ';
+        }
 
-	protected function createRow(array $row) : string
-	{
-		$output = $this->table_separator . ' ';
-		// Only create as many columns as the minimal number of elements
-		// in all rows. Otherwise this would not be a valid Markdown table
-		for ($i = 0; $i < $this->length; ++$i) {
-			$element = $this->padded(trim($row[$i]), $this->col_widths[$i]);
-			$output .= $element;
-			$output .= ' ' . $this->table_separator . ' ';
-		}
+        return trim($output) . "\n";
+    }
 
-		$output = trim($output) . "\n"; // row ends with a newline
-		return $output;
-	}
+    /**
+     * Add padding to a string.
+     */
+    private function padded(string $str, int $width): string
+    {
+        if ($width < strlen($str)) {
+            return $str;
+        }
+        $padding_length = $width - strlen($str);
+        $padding        = str_repeat(' ', $padding_length);
 
-	private function minRowLength(array $arr) : int
-	{
-		$min = PHP_INT_MAX;
-		foreach ($arr as $row) {
-			$row_length = count($row);
-			if ($row_length < $min) {
-				$min = $row_length;
-			}
-		}
+        return $str . $padding;
+    }
 
-		return $min;
-	}
+    private function minRowLength(array $arr): int
+    {
+        $min = PHP_INT_MAX;
+        foreach ($arr as $row) {
+            $row_length = count($row);
+            if ($row_length < $min) {
+                $min = $row_length;
+            }
+        }
 
-	/*
-	 * Calculate the maximum width of each column in characters
-	 */
-	private function maxColumnWidths(array $arr) : array
-	{
-		// Set all column widths to zero.
-		$column_widths = array_fill(0, $this->length, 0);
-		foreach ($arr as $row) {
-			foreach ($row as $k => $v) {
-				if ($column_widths[$k] < strlen($v)) {
-					$column_widths[$k] = strlen($v);
-				}
-				if ($k == $this->length - 1) {
-					// We don't need to look any further since these elements
-					// will be dropped anyway because all table rows must have the
-					// same length to create a valid Markdown table.
-					break;
-				}
-			}
-		}
+        return $min;
+    }
 
-		return $column_widths;
-	}
+    // Calculate the maximum width of each column in characters
+    private function maxColumnWidths(array $arr): array
+    {
+        // Set all column widths to zero.
+        $column_widths = array_fill(0, $this->length, 0);
+        foreach ($arr as $row) {
+            foreach ($row as $k => $v) {
+                if ($column_widths[$k] < strlen($v)) {
+                    $column_widths[$k] = strlen($v);
+                }
+                if ($k == $this->length - 1) {
+                    // We don't need to look any further since these elements
+                    // will be dropped anyway because all table rows must have the
+                    // same length to create a valid Markdown table.
+                    break;
+                }
+            }
+        }
 
-	public function getMarkup() : string
-	{
-		return $this->header . $this->rows;
-	}
+        return $column_widths;
+    }
 }
 
 $task = filter_input(INPUT_POST, 'task', FILTER_SANITIZE_STRING);
 
-if ($task == 'convert') {
-	// Retrieve the CSV content
-	$csv = base64_decode(filter_input(INPUT_POST, 'csv', FILTER_SANITIZE_STRING));
+if ('convert' == $task) {
+    // Retrieve the CSV content
+    $csv = base64_decode(filter_input(INPUT_POST, 'csv', FILTER_SANITIZE_STRING));
 
-	// Delimiters between columns (, or ; or ...)
-	$delim = base64_decode(filter_input(INPUT_POST, 'delim', FILTER_SANITIZE_STRING));
-	if (trim($delim) == '') {
-		$delim = ',';
-	}
+    // Delimiters between columns (, or ; or ...)
+    $delim = base64_decode(filter_input(INPUT_POST, 'delim', FILTER_SANITIZE_STRING));
+    if ('' == trim($delim)) {
+        $delim = ',';
+    }
 
-	// In case of the text are between quotes like, for instance,
-	//   "field1","field2", ...   If so, mention '"' as value for $enclosure
-	$enclosure = base64_decode(filter_input(INPUT_POST, 'enclosure', FILTER_SANITIZE_STRING));
+    // In case of the text are between quotes like, for instance,
+    //   "field1","field2", ...   If so, mention '"' as value for $enclosure
+    $enclosure = base64_decode(filter_input(INPUT_POST, 'enclosure', FILTER_SANITIZE_STRING));
 
-	// Separator to use in markdown to separate columns ('|' is the standard)
-	$separator = base64_decode(filter_input(INPUT_POST, 'separator', FILTER_SANITIZE_STRING));
-	if (trim($separator) == '') {
-		$separator = '|';
-	}
+    // Separator to use in markdown to separate columns ('|' is the standard)
+    $separator = base64_decode(filter_input(INPUT_POST, 'separator', FILTER_SANITIZE_STRING));
+    if ('' == trim($separator)) {
+        $separator = '|';
+    }
 
-	// Transpose will works ONLY when there is two records in the
-	// input string. Instead of having a long "horizontal" table, convert
-	// the table vertically. The result table will have two columns and as
-	// many rows that there was columns in the string
-	$bTranspose = boolval(filter_input(INPUT_POST, 'transpose', FILTER_VALIDATE_BOOLEAN));
+    // Transpose will works ONLY when there is two records in the
+    // input string. Instead of having a long "horizontal" table, convert
+    // the table vertically. The result table will have two columns and as
+    // many rows that there was columns in the string
+    $bTranspose = boolval(filter_input(INPUT_POST, 'transpose', FILTER_VALIDATE_BOOLEAN));
 
-	// Create a new CSV parser
-	$parser = new CSVTable($csv, $delim, $enclosure, $separator, $bTranspose);
+    // Create a new CSV parser
+    $parser = new CSVTable($csv, $delim, $enclosure, $separator, $bTranspose);
 
-	// Create a Markdown table from the parsed input
-	header('Content-Type: text/html');
-	echo $parser->getMarkup();
+    // Create a Markdown table from the parsed input
+    header('Content-Type: text/html');
+    echo $parser->getMarkup();
 
-	die();
+    die();
 }
 
 // Sample and default values
 $csv =
-	"Column 1 Header,Column 2 Header\n" .
-	"Row 1-1,Row 1-2\n" .
-	'Row 2-1,Row 2-2';
+    "Column 1 Header,Column 2 Header\n" .
+    "Row 1-1,Row 1-2\n" .
+    'Row 2-1,Row 2-2';
 
-$delim = ',';
+$delim     = ',';
 $enclosure = ',';
 $separator = '|';
 
 // Get the GitHub corner
 $github = '';
 if (is_file($cat = __DIR__ . DIRECTORY_SEPARATOR . 'octocat.tmpl')) {
-	$github = str_replace('%REPO%', REPO, file_get_contents($cat));
+    $github = str_replace('%REPO%', REPO, file_get_contents($cat));
 }
 ?>
 
@@ -331,6 +332,24 @@ if (is_file($cat = __DIR__ . DIRECTORY_SEPARATOR . 'octocat.tmpl')) {
 			<div class="container">
 
 				<div class="form-group">
+					<details>
+						<summary>How to use?</summary>
+
+						<div class="row">
+								<div class="col-sm">
+									<ul>
+										<li>Copy/Paste your CSV content in the textbox below</li>
+										<li>Update one or more options</li>
+										<li>If you've only two lines, you can select Transpose</li>
+										<li>Click on the Convert button</li>
+									</ul>
+								</div>
+								<div class="col-sm">
+									<img height="300px" src="https://raw.githubusercontent.com/cavo789/marknotes_csv2md/master/images/demo.gif" alt="Demo">
+								</div>
+							</div>
+						</div>
+					</details>
 					<label for="csv">Copy/Paste your CSV content in the textbox below then click on the Convert button:</label>
 					<textarea class="form-control" rows="5" id="csv" name="csv"><?php echo $csv; ?></textarea>
 				</div>
